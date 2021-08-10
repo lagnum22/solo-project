0168a98b7e0e29bc9979c402f368ba4714e8c8a5
wmungal <lagnum22@gmail.com>
2021-08-10 19:50:24 -0400

    modified react app in client folder, added css

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {}
+
+  render() {
+    return <div>Hello Wes! You have successfully bundled!</div>;
+  }
+}
+
+export default App;
