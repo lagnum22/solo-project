2f7a40519bbeac44d7477d49ccf1f25720248ecb
wmungal <lagnum22@gmail.com>
2021-08-10 20:28:50 -0400

    npm installed babelpack and began configing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 
 module.exports = {
-    entry: '.client/index/js'
+  entry: "./client/index.js", //initial file that webpack parses through to make a graph
+  output: {
+    path: path.resolve(__dirname, "build"), //tells webpack where to emit the bundles(not created until you run build)
+    filename: "bundle.js",
+  },
+  mode: "production", //this is default value- we are creating a minified and ugly bundle to make faster code delivery
 }; //closes module.expots
